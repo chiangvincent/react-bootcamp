82549a8bb3c3b39c334c8f682e0f57eae43b8b96
Vincent Chiang <vincentchiang@berkeley.edu>
2020-06-02 11:56:43 -0700

    added home and about components

---
+++ app/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Home.js';
+import About from './About.js';
+import Topics from './Topics.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <Home />
+      <About />
+      <Topics />
+    </div>
+  );
+}
+
+export default App;
